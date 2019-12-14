a670dde2af09411d12ee712786d7374621583061
dyyyl <hey@dyyyl.rocks>
2019-12-14 01:29:55 +0100

    adds endpoint to create query single note document

---
+++ get.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+// eslint-disable-next-line
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call('get', params);
+    // Return the retrieved item
+    if (result.Item) return success(result.Item);
+    // ...or not
+    else return failure({ status: false, error: 'Item not found.' });
+  } catch (error) {
+    return failure({ status: false });
+  }
+}
